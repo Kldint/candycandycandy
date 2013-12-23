aaca8d31cd64ca47c44c782ef95997f43f574085
Fira <loyauflorian@gmail.com>
2013-12-23 22:26:42 +0100

    Added functions for combination linked lists

--- combi.c
+++ combi.c
@@ -5,7 +5,20 @@ void appendCombiList(combi** list, combi* element) {
 		return;
 	}
 	cursor = (*list);
-	while(cursor->next) {}
+	while(cursor->next) { cursor = cursor->next; }
 	cursor->next = element;
 }
 	
+void removeCombiList(combi** list, combi* element) {
+	combi* cursor;
+	
+	if(!list) return;
+	cursor = (*list);
+	while(cursor->next && cursor->next != element) {}
+	if(cursor->next == element) {
+		cursor->next = element->next;
+		element->next = NULL;
+	}
+	
+	return;
+}
